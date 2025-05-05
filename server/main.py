0d5e72e6d04cee2dc0beb2ead27c4a83e5ac71f2
Anthony <acarpriau@users.noreply.github.com>
2025-05-05 15:52:51 +0200

    v6.1.6b

---
+++ server/main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+import requests
+import json
+import os
+
+PREFIX = os.getenv("AMQC_PREFIX", "")
+
+app = FastAPI()
+
+# Dynamically detect the static directory relative to this script
+base_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(base_dir, "static")
+
+if not os.path.exists(static_dir):
+    raise RuntimeError(f"Static directory '{static_dir}' does not exist")
+
+# Mount static files under /AMQC (with optional prefix)
+app.mount(f"{PREFIX}/AMQC", StaticFiles(directory=static_dir), name="static")
+
+
+@app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
+async def req1(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost"
+                                , headers=headers, data={})    
+    #print("====>"*30)
+    #print("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
+    #print(response.text)
+    #print(request.headers)
+    return response.json()
+
+@app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*")
+async def req2(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*"
+                                , headers=headers, data={})    
+    return response.json()
+
+@app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*")
+async def req3(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*"
+                                , headers=headers, data={})    
+    return response.json()
+
+@app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*")
+async def req4(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*"
+                                , headers=headers, data={})    
+    return response.json()
+
+@app.post(f"{PREFIX}/api/jolokia")
+async def jolpost(request:Request ):
+    #print("====>"*30)
+    body=await request.json()
+    #print(body)
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("POST", "http://localhost:8161/api/jolokia", headers=headers, data=json.dumps(body))
+    return response.json()
+
+@app.post(f"{PREFIX}/api/message/{{target}}")
+async def jolmessage(request:Request,target,type,Origin,ID ):
+    #print("====>"*30)
+    body=await request.body()
+    #print(body)
+    #print(type)
+    url=f"http://localhost:8161/api/message/{target}?type={type}&Origin={Origin}&ID={ID}"
+    #print(url)
+    #print(request.headers)
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("POST", url
+                                , headers=headers, data=body.decode("utf-8"))
+    
+    print(response.text)
+    return {"result":response.text}
+
